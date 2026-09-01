1613df2a06762d7a259aa8a24e89aaec8312f732
Your Name <your-email@example.com>
2026-09-01 19:59:41 +0530

    gemini 3.6

--- server/controllers/diseaseController.js
+++ server/controllers/diseaseController.js
@@ -242,7 +242,7 @@ If the plant is healthy, return:
     // --------------------------------------------------
 
     const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
+      model: "gemini-3.6-flash",
 
       contents: [
         {
